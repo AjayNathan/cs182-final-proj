ab0522bd9ea4173de56bacb160ea34751f661a22
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-04 20:49:21 -0500

    minor

--- vgg-16.py
+++ vgg-16.py
@@ -68,10 +68,10 @@ def model():
     model.add_input(name='image', input_shape=(140,37,1))
     model.add_node(ZeroPadding2D((1,1)), name='zp', input='image')
 
-    model.add_node(Convolution2D(1024, 7, 7, activation='relu'), name='c1', input='zp')
+    model.add_node(Convolution2D(1024, 3, 3, activation='relu'), name='c1', input='zp')
     model.add_node(MaxPooling2D((3,3), strides=(1,1)), name='mp1', input='c1')
     
-    model.add_node(Convolution2D(1024, 7, 7, activation='relu'), name='c2', input='mp1')
+    model.add_node(Convolution2D(1024, 3, 3, activation='relu'), name='c2', input='mp1')
     model.add_node(MaxPooling2D((3,3), strides=(1,1)), name='mp2', input='c2')    
 
     model.add_node(Convolution2D(1024, 3, 3, activation='relu'), name='c3', input='mp2')
