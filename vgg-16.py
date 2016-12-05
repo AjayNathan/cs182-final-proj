ff03e2de74a70b4792556b9a03425f7a62b9187e
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-04 20:39:59 -0500

    minor

--- vgg-16.py
+++ vgg-16.py
@@ -160,10 +160,10 @@ if __name__ == '__main__':
     Y_train = np_utils.to_categorical(y_train)
     Y_test = np_utils.to_categorical(y_test)
 
-    model = model()
+    model = VGG_16s()
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     print "compiling"
-    model.compile(optimizer=sgd, loss={'output': 'categorical_crossentropy'})
+    model.compile(optimizer="adam", loss={'output': 'categorical_crossentropy'})
     print "compiled"
     model.fit({'image': X_train, 'output': Y_train}, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data={'image': X_test, 'output': Y_test})
 
