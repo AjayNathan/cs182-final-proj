106d909146063d43896c8d6e8d34db08c2552a83
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-05 18:10:50 -0500

    test

--- vgg-16.py
+++ vgg-16.py
@@ -100,13 +100,13 @@ if __name__ == '__main__':
     X_train, X_test, Y_train, Y_test = processData()
 
     # load model from weights and compile
-    model = characterModel('my_weights.h5')
+    model = characterModel()
     model.compile(optimizer="adam", loss={'output': 'categorical_crossentropy'})
 
     # train model and save weights
     # training = 15 epochs * 67s per epoch on Tesla M40 GPU
     model.fit({'image': X_train, 'output': Y_train}, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data={'image': X_test, 'output': Y_test})
-    model.save_weights('my_weights.h5')
+    model.save_weights('my_weights1.h5')
 
     # evaluate model
     print model.evaluate({'image': X_test, 'output': Y_test}, verbose=0)
