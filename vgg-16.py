5c1a42d4b3a143cc068a041fd9c7b4f6db98988a
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-06 16:18:55 -0500

    damn

--- vgg-16.py
+++ vgg-16.py
@@ -87,7 +87,7 @@ def processData():
     x_data = np.concatenate((clinton_tweets, trump_tweets), axis=0)
     y_data = np.concatenate((clinton_y, trump_y), axis=0)
 
-    X_train, X_test, y_train, y_test = sk_split(x_data, y_data, test_size=0.10, random_state=41)
+    X_train, X_test, y_train, y_test = sk_split(x_data, y_data, test_size=0.05, random_state=41)
 
     Y_train = np_utils.to_categorical(y_train)
     Y_test = np_utils.to_categorical(y_test)
