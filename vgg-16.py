a0c6b5e57d1cf9c71d8bf5c6112f9b426d20bc22
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-06 16:43:11 -0500

    small

--- vgg-16.py
+++ vgg-16.py
@@ -96,7 +96,7 @@ def processData():
 
 if __name__ == '__main__':
     batch_size = 128
-    nb_epoch = 1
+    nb_epoch = 10
 
     # process data
     X_train, X_test, Y_train, Y_test, y_test = processData()
