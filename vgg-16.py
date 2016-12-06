cb84025f492d0824b8a8ef21544a28caf9f601b2
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-06 17:37:54 -0500

    swag:

--- vgg-16.py
+++ vgg-16.py
@@ -18,12 +18,12 @@ def characterModel(weights_path = None):
     model.add_input(name='image', input_shape=(140,63,1))
 
     # convolution layers
-    model.add_node(Convolution2D(8, 1, 1, activation='relu'), name='c1', input='image')
+    model.add_node(Convolution2D(32, 1, 1, activation='relu'), name='c1', input='image')
     
     # two fully-connected layers
     model.add_node(Flatten(), name='f', input='c1')
     model.add_node(Dense(2048, activation='relu'), name='d1', input='f')
-    model.add_node(Dropout(0.75), name='dr1', input='d1')
+    model.add_node(Dropout(0.9), name='dr1', input='d1')
     model.add_node(Dense(2, activation='softmax'), name='d2', input='dr1')
 
     # output
