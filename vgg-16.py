e662f15b7c5c6ea185a538b42924ebedc34ff6d3
Ajay Nathan <ajaynathan@college.harvard.edu>
2016-12-06 15:49:17 -0500

    more r

--- vgg-16.py
+++ vgg-16.py
@@ -18,7 +18,7 @@ def characterModel(weights_path = None):
     model.add_input(name='image', input_shape=(140,63,1))
 
     # convolution layers
-    model.add_node(Convolution2D(32, 1, 1, activation='relu', W_regularizer=l2(10)), name='c1', input='image')
+    model.add_node(Convolution2D(32, 1, 1, activation='relu', W_regularizer=l2(1), b_regularizer=l2(0.1)), name='c1', input='image')
     
     # two fully-connected layers
     model.add_node(Flatten(), name='f', input='c1')
